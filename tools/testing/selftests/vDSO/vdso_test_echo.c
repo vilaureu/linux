251f3a1e96907ec633621fd3d81db7ebecfcf760
vilaureu <vilaureu@users.noreply.github.com>
2021-03-10 11:09:55 +0100

    test vDSO echo

---
+++ tools/testing/selftests/vDSO/vdso_test_echo.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * vdso_test_echo.c: Sample code to test parse_vdso.c and vDSO echo()
+ */
+
+#include <stdint.h>
+#include <elf.h>
+#include <stdio.h>
+#include <sys/auxv.h>
+#include <sys/time.h>
+
+#include "../kselftest.h"
+#include "parse_vdso.h"
+
+#define MSG ((long) 0x1337)
+
+const char *version = "LINUX_2.6";
+const char *name = "__vdso_echo";
+
+typedef long (*echo_t)(long);
+
+int main(int argc, char **argv)
+{
+	unsigned long sysinfo_ehdr;
+	unsigned int cpu, node;
+	echo_t echo;
+	long ret;
+
+	sysinfo_ehdr = getauxval(AT_SYSINFO_EHDR);
+	if (!sysinfo_ehdr) {
+		printf("AT_SYSINFO_EHDR is not present!\n");
+		return KSFT_SKIP;
+	}
+
+	vdso_init_from_sysinfo_ehdr(getauxval(AT_SYSINFO_EHDR));
+
+	echo = (echo_t)vdso_sym(version, name);
+	if (!echo) {
+		printf("Could not find %s\n", name);
+		return KSFT_SKIP;
+	}
+
+  ret = echo(MSG);
+	if (MSG != ret) {
+		printf("Expected %ld found %ld\n", MSG, ret);
+		return KSFT_FAIL;
+  }
+
+	return 0;
+}

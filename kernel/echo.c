926258159e562e576ce6c0621030cb8fd41fc5f1
vilaureu <vilaureu@users.noreply.github.com>
2021-03-11 21:35:57 +0100

    update documentation for the echo systemcall

--- kernel/echo.c
+++ kernel/echo.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /**
- * Syscall that simply returns the long input argument msg
+ * Syscalls to implement offset dependend echo functionality
  */
 #include <asm-generic/rwonce.h>
 #include <linux/syscalls.h>
@@ -8,11 +8,18 @@
 
 long _vdso_echo_off __attribute__((section(".vvar__vdso_echo_off"))) __visible;
 
+/*
+ * The echo systemcalls returns the input msg plus the echo offset
+ */
 SYSCALL_DEFINE1(echo, long, msg)
 {
 	return msg + READ_ONCE(_vdso_echo_off);
 }
 
+/*
+ * Set the echo offset systemwide.
+ * This systemcall does not check for any permissions.
+ */
 SYSCALL_DEFINE1(echo_offset, long, off)
 {
 	WRITE_ONCE(_vdso_echo_off, off);

d24e61ec381e94121c5ddcc72cf692ded717e02d
vilaureu <vilaureu@users.noreply.github.com>
2022-01-27 11:53:52 +0100

    implement interception of fastcall system calls on ARM64

--- arch/arm64/include/asm/fastcall.h
+++ arch/arm64/include/asm/fastcall.h
@@ -2,10 +2,15 @@
 #ifndef _ASM_FASTCALL_H
 #define _ASM_FASTCALL_H
 
+#define FASTCALL_SVC_IMM 0xFC
+
+#ifndef __ASSEMBLER__
+
 static inline bool in_fastcall_region(unsigned long start, size_t len)
 {
   // TODO
 	return false;
 }
 
+#endif /* !__ASSEMBLER__ */
 #endif /* _ASM_FASTCALL_H */

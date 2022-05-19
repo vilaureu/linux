26d86a13be231fff555a5c341601775c8f3585d9
vilaureu <vilaureu@users.noreply.github.com>
2022-05-19 09:05:24 +0200

    use hand-written asm for syscall bench function
    This should prevent compiler optimizations and unify the assembly across
    measurement points.

--- drivers/syscall-bench/syscall_bench.c
+++ drivers/syscall-bench/syscall_bench.c
@@ -12,12 +12,19 @@
  */
 SYSCALL_DEFINE2(syscall_bench, uint32_t, idx, uint64_t __user *, measurements)
 {
-	unsigned int eax, ebx, ecx, edx;
-	unsigned long long counter;
+	uint32_t eax = 0;
+	uint64_t counter;
 
-	cpuid(0, &eax, &ebx, &ecx, &edx);
-	counter = native_read_pmc(idx);
-	cpuid(0, &eax, &ebx, &ecx, &edx);
+	asm volatile("cpuid;"
+		     "movl %2, %%ecx;"
+		     "rdpmc;"
+		     "salq $32, %%rdx;"
+		     "leaq (%%rdx, %%rax), %1;"
+		     "xorl %%eax, %%eax;"
+		     "cpuid;"
+		     : "+&a"(eax), "=r"(counter)
+		     : "r"(idx)
+		     : "ebx", "ecx", "edx", "memory");
 
 	if (copy_to_user(&measurements[4], &counter, sizeof(counter)))
 		return -EFAULT;
